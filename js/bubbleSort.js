08caba4202431df9806fae9c2fbe99e1cede6e36
David O'Mullan <davidomullan@gmail.com>
2023-01-02 16:22:38 -0600

    Implemented bubble sort and displayed with present array.

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -1,5 +1,5 @@
 var xValues = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var yValues = [ 5, 1, 9, 2, 3, 7, 8, 4, 0, 6];
+var yValues = [ 5, 1, 9, 8, 2, 3, 7, 4, 0, 6];
 // var barColors = ["red", "green", "blue", "orange", "brown", "teal", 'magenta', "maroon", "yellow", "black"];
 
 var bubbleSortChart = new Chart("bubbleSortChart", {
@@ -28,8 +28,55 @@ var bubbleSortChart = new Chart("bubbleSortChart", {
   }
 });
 
-function bubbleSort(){
-  yValuesNew = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  bubbleSortChart.data.datasets[0].data = yValuesNew;
+function bubbleSort(arr, n){
+
+  //base case
+  if (n==1)
+    return;
+
+  var count = 0;
+
+  for(var i = 0; i<n-1; i++){
+    if(arr[i] > arr[i+1]){
+      var temp = arr[i];
+      arr[i] = arr[i+1];
+      arr[i+1] = temp;
+      count++;
+    }
+  }
+
+  if(count == 0){
+    return;
+  }
+
+  bubbleSortChart.data.datasets[0].data = arr;
+  // console.log('New array: ', arr);
   bubbleSortChart.update();
+
+  setTimeout(() => {
+    requestAnimationFrame(() => {
+      bubbleSort(arr, n-1);
+    });
+  }, 800);
+
 }
+
+
+/* For loop will not sync with frames to update chart...
+
+function bubbleSort(arr){
+  for(var i = 0; i < arr.length; i++){
+    for(var j = 0; j < (arr.length-i-1); j++){
+      if(arr[j] > arr[j+1]){
+        var temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+
+      bubbleSortChart.data.datasets[0].data = arr;
+      console.log('New array: ', arr);
+      bubbleSortChart.update();
+    }
+  }
+}
+*/
